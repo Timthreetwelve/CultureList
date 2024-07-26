3a013cd197f3cea80911afa419ec85dae098a783
Tim Kennedy <timthreetwelve@outlook.com>
2024-07-26 15:44:31 -0500

    Changed combo box to radio buttons.

---
+++ CultureList/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace CultureList.Converters;
+
+/// <summary>
+/// Converts an enum value to a boolean.
+/// </summary>
+internal class EnumToBooleanConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value!.Equals(parameter);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (bool)value! ? parameter! : Binding.DoNothing;
+    }
+}
